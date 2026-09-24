c3ad4b720ede891e8eb79028d594ed9d05baef4b
nick-sawhney <nick.sawhney@evolutioniq.com>
2026-09-24 00:10:10 -0400

    fix(types): correct image-url import and typecheck scope

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,6 +1,5 @@
 import { sanityClient } from 'sanity:client'
-import { createImageUrlBuilder } from '@sanity/image-url'
-import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
+import { createImageUrlBuilder, type SanityImageSource } from '@sanity/image-url'
 
 const builder = createImageUrlBuilder(sanityClient)
 
